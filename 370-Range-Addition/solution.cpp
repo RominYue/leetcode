65c69a7722bc4feed9d07adf388a0df4a29942d8
RominYue <yueming412@gmail.com>
2016-09-14 10:45:40 +0800

    [370. Range Addition][Accepted]committed by LeetCode Extension

---
+++ 370-Range-Addition/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        vector<int> ret(length, 0);
+        
+        for(int i = 0; i < updates.size(); i++)
+        {
+            int l = updates[i][0];
+            int r = updates[i][1];
+            int inc = updates[i][2];
+            if(l < length) ret[l] += inc;
+            if(r + 1 < length) ret[r+1] -=inc;
+        }
+        
+        int curinc = 0;
+        
+        for(int i = 0; i < length; i++)
+        {
+            int tmp = ret[i];
+            ret[i] += curinc;
+            curinc += tmp;
+        }
+        
+        return ret;
+    }
+};
